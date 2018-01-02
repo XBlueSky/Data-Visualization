f9e797abf25cae85d525e0e6846dc042123320df
XBlueSky <30610447+XBlueSky@users.noreply.github.com>
2018-01-02 14:51:27 +0800

    Add files via upload

---
+++ 2Dnot.py
@@ -0,0 +1,124 @@
+import csv
+import numpy as np
+import pandas as pd
+from time import time
+from bokeh.models import BoxAnnotation
+from bokeh.client import push_session
+from bokeh.plotting import figure, curdoc
+from bokeh.layouts import WidgetBox, column, row
+from bokeh.models.widgets import Panel, Tabs, Select
+
+
+#filename = input("Enter a file name: ")
+filename = 'stocks.csv'
+file = open(filename, 'r')
+csvCursor = csv.reader(file)
+
+is_header = True
+input_data = dict()
+header = []
+
+start = time()
+print ('start time: ', start)
+
+# declare figure #     
+p1 = figure(toolbar_location=None,output_backend="canvas", plot_height=1000, plot_width=1000)
+p1.xaxis.major_label_orientation = np.pi/4
+# p2 = figure(toolbar_location=None)
+# p2.xaxis.major_label_orientation = np.pi/4
+# p3 = figure(toolbar_location=None,output_backend="canvas")
+# p3.xaxis.major_label_orientation = np.pi/4
+
+# open a session to keep our local document in sync with server
+#session = push_session(curdoc())
+
+for r in csvCursor:
+    if is_header:
+        # store keys in dict #
+        header = r
+        for i in range(len(r)):
+            input_data[r[i]] = []
+
+        # change labels of x-axes and y-axes
+        p1.xaxis.axis_label = header[0]
+        # p2.xaxis.axis_label = header[0]
+        # p3.xaxis.axis_label = header[0]
+        #p.yaxis.axis_label = header[1]
+
+        is_header = False
+    else:
+        #if test>0:
+        for i in range(len(r)):
+            if i==0: # store year #
+                tmp = r[i].split('-')
+                year = ''
+                for it in tmp:
+                    year += it
+                input_data[header[i]].append(int(year))
+            else:
+                input_data[header[i]].append(float(r[i]))
+        #test -= 1
+
+start_i = end_i = 0
+# start_j = end_j = 0
+# start_k = end_k = 0
+
+
+cir = p1.circle(x=[], y=[])
+# bar = p2.vbar(x=[],top=[], width=0.5)
+# lin = p3.circle(x=[], y=[])
+
+dc = cir.data_source
+# db = bar.data_source
+# dl = lin.data_source
+
+def cir_callback():
+    global start_i, end_i, c
+    if end_i<len(input_data['Date']):
+
+        new_data = dict()
+        new_data['x'] = dc.data['x'] + input_data['Date'][start_i:end_i]
+        new_data['y'] = dc.data['y'] + input_data['Open'][start_i:end_i]
+        # print (int(input_data['Date'][start_i])/10000)
+        if int((input_data['Date'][start_i])/10000) >= 1988:
+            box = BoxAnnotation(left=input_data['Date'][start_i], right=input_data['Date'][end_i], fill_color='red', fill_alpha=0.1)
+            p1.add_layout(box)
+        
+        # if int(input_data['Date'][start_i]/10000) <= 1962:
+            
+        #     end = time()
+        #     print ('total time: ', end-start)
+
+        dc.data = new_data
+        start_i = end_i
+        end_i += 50
+
+# def bar_callback():
+#     global start_j, end_j
+#     new_data = dict()
+#     new_data['x'] = db.data['x'] + input_data['Date'][start_j:end_j]
+#     new_data['top'] = db.data['top'] + input_data['Open'][start_j:end_j]
+    
+#     db.data = new_data
+
+#     start_j = end_j
+#     end_j += 50
+
+# def lin_callback():
+#     global start_k, end_k
+#     new_data = dict()
+#     new_data['x'] = dl.data['x'] + input_data['Date'][start_k:end_k]
+#     new_data['y'] = dl.data['y'] + input_data['Open'][start_k:end_k]
+
+#     dl.data = new_data
+
+#     start_k = end_k
+#     end_k += 50
+
+curdoc().add_periodic_callback(cir_callback, 50)
+# curdoc().add_periodic_callback(bar_callback, 50)
+# curdoc().add_periodic_callback(lin_callback, 50)
+
+curdoc().add_root(p1)
+#session.show(tabs)
+#session.loop_until_closed() # run forever
--- miws.py
+++ miws.py
@@ -1,7 +1,12 @@
 import random
 from bokeh.client import push_session
 from bokeh.plotting import figure, curdoc
+from bokeh.models import BoxAnnotation
+from numba import jit
 import time
+import numpy as np
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import cpu_count
 
 class Node:
     def __init__(self, index, weight, adjVec = []):
@@ -27,7 +32,7 @@ weightList = []
 graphList = []
 MWIS = []
 currentState = []
-stopState = []
+lastState = []
 totalWeight = 0
 ranInOut = [0, 1]
 infiniteFlag = False
@@ -39,7 +44,7 @@ for i,line in enumerate(file):
             weightList.append( int(plot))
     elif i > 1:
         graphList.append( Node( i-2, weightList[i-2], list( map( int, line.split()))))
-        stopState.append(-1)
+        lastState.append(-1)
         currentState.append(random.choice(ranInOut))
 file.close()
 
@@ -47,40 +52,89 @@ file.close()
 # declare figure #  
 
 
-p = figure(toolbar_location=None)
+#p = figure(toolbar_location=None,output_backend="webgl")
+p = figure(toolbar_location=None, plot_height=1000, plot_width=1000)
 r = p.circle(x=[], y=[], color="skyblue", line_width=4, size=25)
+box = BoxAnnotation(top=1.3, bottom=0.5, fill_color='pink', fill_alpha=0.5)
+p.add_layout(box)
 
 ds = r.data_source
 
 
 # count = 1
 
-# while currentState !=  stopState:
-def compute():
-    global currentState, graphList, stopState
-    randomIndex = random.randint(0,len(currentState)-1)
-    for adjNode in graphList[randomIndex].getAdjNode():
-        if currentState[adjNode] == 1 and graphList[randomIndex].weightPerNumPlusOne <= graphList[adjNode].weightPerNumPlusOne:
+def inChange(node):
+    global currentState, graphList, lastState
+    for adjNode in node.getAdjNode():
+        if lastState[adjNode] == 1 and node.weightPerNumPlusOne < graphList[adjNode].weightPerNumPlusOne:
             flag = 0
             break
-        else:
-            flag = 1
-    if flag == 1:
-        currentState[randomIndex] = 1
-    elif flag == 0:
-        currentState[randomIndex] = 0     
-
-    for node in graphList:
-        for adjNode in node.getAdjNode():
-            if currentState[adjNode] == 1 and node.weightPerNumPlusOne <= graphList[adjNode].weightPerNumPlusOne:
+        elif lastState[adjNode] and node.weightPerNumPlusOne == graphList[adjNode].weightPerNumPlusOne:
+            if node.index < graphList[adjNode].index:
                 flag = 0
                 break
             else:
                 flag = 1
-        if flag == 1:
-            stopState[node.index] = 1
-        elif flag == 0:
-            stopState[node.index] = 0
+        else:
+            flag = 1
+    if flag == 1:
+        currentState[node.index] = 1
+        #return 1
+    elif flag == 0:
+        currentState[node.index] = 0
+        #return 0
+
+# while currentState !=  stopState:
+
+def compute():
+    global currentState, graphList, lastState
+
+    lastState = currentState[:]
+    for node in graphList:
+    # pool = ProcessPoolExecutor(max_workers=cpu_count())  
+    # currentState = list(pool.map(inChange,graphList))
+        inChange(node)
+        # for adjNode in node.getAdjNode():
+        #     if lastState[adjNode] == 1 and node.weightPerNumPlusOne < graphList[adjNode].weightPerNumPlusOne:
+        #         flag = 0
+        #         break
+        #     elif lastState[adjNode] and node.weightPerNumPlusOne == graphList[adjNode].weightPerNumPlusOne:
+        #         if node.index > graphList[adjNode].index:
+        #             flag = 0
+        #             break
+        #         else:
+        #             flag = 1
+        #     else:
+        #         flag = 1
+        # if flag == 1:
+        #     currentState[node.index] = 1
+        # elif flag == 0:
+        #     currentState[node.index] = 0
+
+
+    # randomIndex = np.random.randint(0,len(currentState)-1)
+    # for adjNode in graphList[randomIndex].getAdjNode():
+    #     if currentState[adjNode] == 1 and graphList[randomIndex].weightPerNumPlusOne <= graphList[adjNode].weightPerNumPlusOne:
+    #         flag = 0
+    #         break
+    #     else:
+    #         flag = 1
+    # if flag == 1:
+    #     currentState[randomIndex] = 1
+    # elif flag == 0:
+    #     currentState[randomIndex] = 0   
+
+    # for node in graphList:
+    #     for adjNode in node.getAdjNode():
+    #         if currentState[adjNode] == 1 and node.weightPerNumPlusOne <= graphList[adjNode].weightPerNumPlusOne:
+    #             flag = 0
+    #             break
+    #         else:
+    #             flag = 1
+    #     if flag == 1:
+    #         stopState[node.index] = 1
+    #     elif flag == 0:
+    #         stopState[node.index] = 0
     # if count > len(currentState)*100:
     #     infiniteFlag = True
     #     break
@@ -102,5 +156,5 @@ def compute():
 # print(totalWeight)
 
 
-curdoc().add_periodic_callback(compute, 100)
+curdoc().add_periodic_callback(compute, 500)
 curdoc().add_root(p)
